baa3cdc1e7f85dd4278b145ea4a081ac81ba0456
vellasques35githuub <fabio_vellasques@hotmail.com>
2025-04-09 23:23:01 -0300

    Atualização App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,12 +1,39 @@
-﻿namespace MedicacaoDiariaApp
+﻿using MauiAppMinhasCompras.Helpers;
+using System.Globalization;
+
+namespace MedicacaoDiariaApp
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if(_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_medicamentos.db3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+
+                return _db;
+            }
+
+
+        }
         public App()
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+             Thread.CurrentThread.CurrentCulture = new CultureInfo("pt-BR");
+
+            //MainPage = new AppShell();
+            MainPage = new NavigationPage(new Views.ListaMedicamento());
         }
     }
 }
