6ea1e34550cd4710d1b06d51a1ee26b94fe92257
vellasques35githuub <fabio_vellasques@hotmail.com>
2025-04-09 23:17:36 -0300

    Atualização EditarMedicamento.xaml.cs

--- VIews/EditarMedicamento.xaml.cs
+++ VIews/EditarMedicamento.xaml.cs
@@ -6,4 +6,27 @@ public partial class EditarMedicamento : ContentPage
 	{
 		InitializeComponent();
 	}
+ private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Medicamento medicamento_anexado = BindingContext as Medicamento;
+
+            Medicamento m = new Medicamento
+            {
+                Id = medicamento_anexado.Id,
+                Descricao = txt_descricao.Text,
+               
+	    };
+
+            await App.Db.Update(p);
+            await DisplayAlert("Sucesso!", "Resgistro Atualizado", "OK");
+            await Navigation.PopAsync();
+
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
 }
